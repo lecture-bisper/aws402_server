742d144ad065d73728f7d4cba8ccb78dfbb25620
lecture-bisper <bisper@outlook.kr>
2026-08-14 17:36:34 +0900

    Spring Boot - 스프링부트 게시판 만들기1, Mapper 및 XML 파일 작성하기

---
+++ board2/src/main/java/bitc/aws402/board2/mapper/BoardMapper.java
@@ -0,0 +1,48 @@
+//  File :  bitc.aws402.board2.mapper.BoardMapper.java
+//  User :  it
+//  Date :  2026-08-13
+//  Time :  오후 3:53
+//  Desc :  
+
+package bitc.aws402.board2.mapper;
+
+import bitc.aws402.board2.dto.BoardDTO;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+//  @Mapper : Mybatis 에서 제공하는 어노테이션
+//  xml 파일과 연동되어 실제 데이터베이스에 대한 연결 및 제어를 진행
+//  서비스 계층에 데이터베이스에 사용 방법을 제공함
+
+@Mapper
+public interface BoardMapper {
+
+//  게시글 목록
+  List<BoardDTO> selectBoardList();
+
+//  @Param : 1:1로 연동된 xml 파일의 태그에서 사용할 이름 설정
+//  게시글 상세
+  BoardDTO selectBoardDetail(@Param("boardIdx") int boardIdx);
+
+//  게시글 등록
+  void insertBoard(BoardDTO board);
+
+//  게시글 수정
+  void updateBoard(BoardDTO board);
+
+//  게시글 삭제
+  void deleteBoard(@Param("boardIdx") int boardIdx);
+}
+
+
+
+
+
+
+
+
+
+
+
